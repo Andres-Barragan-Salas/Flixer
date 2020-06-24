41c54c47f14510a61a8c5a036f7eeb9f7f88a87f
Andres Barragan <andresbarragan@fb.com>
2020-06-24 18:19:17 -0500

    App icon, scrolling list and pull-to-refresh core functionalities implemented.

---
+++ Flixer/Views/MovieCell.h
@@ -0,0 +1,21 @@
+//
+//  MovieCell.h
+//  Flixer
+//
+//  Created by Andres Barragan on 24/06/20.
+//  Copyright © 2020 Andres Barragan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLable;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLable;
+
+@end
+
+NS_ASSUME_NONNULL_END
